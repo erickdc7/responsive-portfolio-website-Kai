7a74b88368d58db27c04f81f646f0e87a39187c4
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-12-14 12:55:44 -0500

    change class

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,8 +42,8 @@ const shadowHeader = () => {
     const header = document.getElementById('header')
     // Add a class if the bottom offset is greater than 50 of the viewport
     this.scrollY >= 50
-        ? header.classList.add('scroll-header')
-        : header.classList.remove('scroll-header')
+        ? header.classList.add('shadow-header')
+        : header.classList.remove('shadow-header')
 }
 window.addEventListener('scroll', shadowHeader)
 
